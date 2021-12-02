19b74b11c1ff1276d4b5e169ef63f699f57e6fd7
asidhu9 <asidhu994@gmail.com>
2021-12-02 11:43:17 -0800

    Third Scene
    adding running and jumping animations

--- FallGuysRemake/Assets/ThirdPersonController.cs
+++ FallGuysRemake/Assets/ThirdPersonController.cs
@@ -12,6 +12,18 @@ public class ThirdPersonController : MonoBehaviour
     private float turnSmoothVelocity;
 
     public Transform cam;
+
+    public Animator animator;
+
+    public bool isGrounded = false;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 playerVelocity;
+    public float gravityValue = 9.81f;
+    public float jumpHeight = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,19 +33,52 @@ public class ThirdPersonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Running();
+        Jump();
+    }
+
+    public void Move()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 move = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(move != Vector3.zero)
+        if (move != Vector3.zero)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) *Vector3.forward;
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+    public void Running()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        animator.SetBool("isRunning", vertical != 0 || horizontal != 0);
+    }
+
+    public void Jump()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && playerVelocity.y <= 0)
+        {
+            playerVelocity.y = -2f;
+            animator.SetBool("isJumping", false);
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            animator.SetBool("isJumping", true);
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * 2.0f * gravityValue);
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
